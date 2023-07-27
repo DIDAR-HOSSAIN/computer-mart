b3426f05ccbe16519c56da4bdb4bf77c34dfa59a
unknown <didarhossain018@gmail.com>
2023-07-28 01:55:18 +0600

    added frontend header and footer

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Error404 = () => {
+    return (
+        <div className='flex justify-center items-center h-[100vh]'>
+            <h1 text-center>404 error</h1>
+        </div>
+    );
+};
+
+export default Error404;
+
+
+//SSG:Static Site Generation (SSG) all ssg
+//SSR:server-side rendering pc builder
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
+import RootLayout from "@/components/frontend/RootLayout/RootLayout";
+import Head from "next/head";
 
 
 const Home = () => {
   return (
     <div>
-      
+       <Head>
+        <title>Computer Mart</title>
+        <meta name="description" content="Computer Sale" />
+        <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
+      </Head>
+      <h1 className="text-6xl">This is home</h1>
     </div>
   );
 };
 
 export default Home;
+
+
+
+Home.getLayout = function getLayout(page) {
+  return (
+    <RootLayout>
+      {page}
+    </RootLayout>
+  )
+}
