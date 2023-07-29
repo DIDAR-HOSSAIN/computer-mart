7d5e3c60a4191ce45861e26f6fbeb9d520a62d18
unknown <didarhossain018@gmail.com>
2023-07-29 22:06:11 +0600

    added featured products

--- src/components/frontend/FeaturedProducts/FeaturedProducts.js
+++ src/components/frontend/FeaturedProducts/FeaturedProducts.js
@@ -2,15 +2,41 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 
-const FeaturedProducts = () => {
-  return (
-    <>     
-      
-  </>
-
-  
-  );
-};
+const FeaturedProducts = ({ products }) => {
+    return (
+      <>
+        {Object.keys(products).map((category) => (
+          <div key={category} className="category">
+            <h2>{category}</h2>
+            <div className="products">
+              {products[category].map((product) => (
+                <div key={product.id} className="product">
+                  <Link href={`/product/${product.id}`}>
+                    <figure>
+                      <Image
+                        className="product-image"
+                        src={product.image}
+                        width={300}
+                        height={300}
+                        alt=""
+                      />
+                    </figure>
+                  </Link>
+                  <div className="product-info">
+                    <h3>{product.name}</h3>
+                    <p>{product.description}</p>
+                    <p>Price: {product.price}</p>
+                    <p>Rating: {product.averageRating}</p>
+                    <p>Status: {product.status ? 'In Stock' : 'Out of Stock'}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  };
 
 
 export default FeaturedProducts;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import FeaturedCategory from "@/components/frontend/FeaturedCategory/FeaturedCategory";
+import FeaturedProducts from "@/components/frontend/FeaturedProducts/FeaturedProducts";
 import RootLayout from "@/components/frontend/RootLayout/RootLayout";
 import Head from "next/head";
 
 
-const Home = () => {
+const Home = ({products}) => {
+  console.log('home page upper', products)
+
   return (
     <div>
        <Head>
@@ -14,18 +17,27 @@ const Home = () => {
       </Head>
       </Head>
       <div className="">
-        <h1 className="border-8 text-center lg:text-4xl md:text-4xl font-bold bg-yellow-300 mt-8">Featured Category</h1>
+        <h1 className="border-8 text-center lg:text-4xl md:text-2xl font-bold bg-yellow-300 mt-8">Featured Category</h1>
         </div>
 
         <div className="gap-8 rounded-lg grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
 
-              <FeaturedCategory />
+            <FeaturedCategory />
 
             </div>
 
       <div className="">
-        <h1 className="text-center lg:text-4xl md:text-4xl font-bold bg-yellow-300 mt-8">Featured Products</h1>
+        <h1 className="border-8 text-center lg:text-4xl md:text-2xl font-bold bg-yellow-300 mt-8">Featured Products</h1>
         </div>
+
+        <div className="gap-8 rounded-lg grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+
+            {
+              <FeaturedProducts products={products}  />
+            }
+            
+
+            </div>
 
 
 
@@ -46,4 +58,19 @@ Home.getLayout = function getLayout(page) {
 }
 
 
+export const getStaticProps = async()=>{
+  const res = await fetch('http://localhost:5000/products');
+  // const res2 = await fetch('http://localhost:5000/watches');
+  const data = await res.json();
+  // const data2 = await res2.json();
+  console.log('from Json Server', data)
+  // console.log('from Json Server', data2)
 
+  return {
+    props:{
+      products:data,
+      // productCategory:data2
+    },
+    // revalidate:10,
+  };
+};
