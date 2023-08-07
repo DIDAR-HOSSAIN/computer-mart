5bec46035f1f37e28b59f7c387f768df8a2b15fd
DIDAR-HOSSAIN <didarhossain018@gmail.com>
2023-08-07 17:13:27 +0600

    added cart

--- src/components/frontend/ProductCategories/Processor.js
+++ src/components/frontend/ProductCategories/Processor.js
@@ -2,40 +2,36 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-const Processor = ({allProcessor}) => {
+const Processor = ({ allProcessor }) => {
   const processorCategories = allProcessor.filter((product) => product.category === 'Processor');
-  console.log('only processor product', processorCategories)
 
-  const handleAddProduct = (processorCategories)=>{
-    console.log('from cart',processorCategories)
-    processorCategories?.map((item)=>(
-      console.log(item.name)
-    ))
-
-
-
+  const handleAddProduct = (productId) => {
+    const productWithId = processorCategories.find((item) => item.id === productId);
+    console.log('Product added to cart:', productWithId);
   };
 
-    return (
-        <>
-    {
-     processorCategories?.map((processor)=>(
-      <div key={processor.id} className=" px-10 pt-8">
-      <Link href={"/"}>
-      <figure className='flex justify-center items-center'><Image className='w-32 h-32' src={processor?.image} width={300} height={300} alt=""/></figure>
-      <div className='text-center'>
-           <h2>{processor.name}</h2>
-          <p>Category: {processor.category}</p>
-          <p>Price: {processor.price}</p>
-          <p>Average Rating: {processor.averageRating}</p>
-         </div>
-      </Link>
-      <button onClick={()=>handleAddProduct(processorCategories)} className="btn btn-active btn-accent">Add To Cart</button>
-      </div>
-    ))
-   }
-  </>
-    );
+  return (
+    <>
+      {processorCategories?.map((processor) => (
+        <div key={processor.id} className="px-10 pt-8">
+          <Link href={"/"}>
+            <figure className='flex justify-center items-center'>
+              <Image src={processor?.image} width={300} height={300} alt="" />
+            </figure>
+            <div className='text-center'>
+              <h2>{processor.name}</h2>
+              <p>Category: {processor.category}</p>
+              <p>Price: {processor.price}</p>
+              <p>Average Rating: {processor.averageRating}</p>
+            </div>
+          </Link>
+          <button onClick={() => handleAddProduct(processor.id)} className="btn btn-active btn-accent">
+            Add To Cart
+          </button>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Processor;
